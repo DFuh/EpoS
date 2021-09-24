5de4b03ceebcf93deec306be96c9b4e605ad1832
DFuh <dafu@uni-bremen.de>
2021-09-24 14:14:07 +0200

    minor changes

--- epos/run_par_setup.py
+++ epos/run_par_setup.py
@@ -1,8 +1,14 @@
 '''
 run poar setup in order to create scenario files
 '''
+import sys
 from par import setup_params as sepa
 
 if __name__ =='__main__':
-    filename_super_parameters = 'par_v001.json'
+    if len(sys.argv) <2:
+        print('Using default super_parameters ... ')
+        filename_super_parameters = 'par_v002.json' # Should be user input
+    else:
+        filename_super_parameters = sys.argv[-1]
+
     sepa.ScenarioSetup(filename_super_parameters)
