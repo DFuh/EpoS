deb54cc1a7775dbd2054017163a3fa5af7b7eff7
DFuh <dafu@uni-bremen.de>
2021-09-24 14:13:32 +0200

    implement storage-clc

--- epos/main/simulation.py
+++ epos/main/simulation.py
@@ -16,6 +16,7 @@ import epos.auxf.readingfiles as rf
 import epos.auxf.handlingfiles as hf
 import epos.auxf.handlingdata as hd
 import epos.auxf.faux as fx
+from epos.clc import ctrl
 
 from epos.main import louter
 
@@ -41,34 +42,35 @@ class ElSim():
         self.name = self.prms['scen_name'].replace('Scen','Sim')
         self.tag = uuid.uuid1()
 
-        if full_simu: # No sig needed for testing
-            ### input data
-            self.metadata_sig, self.data_sig = rf.read_in_signal_dataset(self,
-                                        rel_flpth=self.prms['relpth_sig_data'],
-                                        search_key=self.prms['searchkey_sig_metadata'])
-            #print(f'Data head of simulation {self.name}:', self.data_sig.head())
-            # check properties of df ?
-            #hf.ini_logfile(self,)
-
-            ### ini output data
-            self.df0, self.df0_keys, self.lst_pths_out = hd.ini_data_output(self,)
-
-            ### Store actual Params
-            hf.store_simu_params(self, )
-
-            ### ini logging
-            # TODO: pth to logfile hardcoded
+        ### ini logging
         logpth = os.path.join(self.cwd, 'logfiles')
         #lgg, logger_nm = fx.ini_logging(self, pth=logpth)
         #print('Simu -> logger_nm: ', logger_nm)
         #logger = lgg.getLogger(logger_nm)
         logger, logger_nm = fx.ini_logging(self, pth=logpth, notest=full_simu)
         self.logger = logger
+        # TODO: pth to logfile hardcoded
+
+        if full_simu: # No sig needed for testing
+            ### Read Power input data
+            self.metadata_sig, self.data_sig = rf.read_in_dataset(self,
+                                        rel_flpth=self.prms['relpth_sig_data'],
+                                        search_key=self.prms['searchkey_sig_metadata'])
+            ### Read H2-Demand data
+            self.metadata_H2dmnd, self.data_H2dmnd = rf.read_in_dataset(self,
+                                        rel_flpth=self.prms['relpth_H2dmnd_data'],
+                                        search_key=self.prms['searchkey_H2dmnd_metadata'])
+            #print(f'Data head of simulation {self.name}:', self.data_sig.head())
+            # check properties of df ?
+            #hf.ini_logfile(self,)
+
+            ### ini output data
+            self.df0, self.df0_keys, self.lst_pths_out = hd.ini_data_output(self,)
+            self.prms['pathlist_outputfiles'] = self.lst_pths_out
+            ### Store actual Params
+            hf.store_simu_params(self, )
 
         #logger.info('Today_ymdhs: %s', self.today_ymdhs)
-
-
-
 
         #self.calc_modules = fx.ini_clc_versions(self,) # returns tuple
         logger.info('Initialized Simulation: %s', self.name)
@@ -99,8 +101,9 @@ class ElSim():
         self.p = hd.dct_to_nt(par['operation']['nominal_electrode_pressure'],
                                     subkey='value')
         ''' CHECK below !!!'''
-        self.fnct = hd.dct_to_nt(par['functions'],
-                                    subkey='value')
+        if par['functions']:
+            self.fnct = hd.dct_to_nt(par['functions'],
+                                        subkey='value')
 
         #print('self.pop: ', self.pop)
         #print('self.pcll:', self.pcll)
@@ -114,6 +117,10 @@ class ElSim():
         ### Setup auxilliary values (dataclass)
         hd.ini_auxvals(self, par)
 
+        ### ??? Setup PID cntrl ->> ??? here ???
+        self.pid_ctrl = ctrl.PID_controller()
+        self.pid_ctrl.reset()
+        self.pid_ctrl.tune_man(*self.bop.pid_parameters)
 
         return
 
@@ -122,13 +129,29 @@ class ElSim():
         self.logger.info('Run Simulation: %s', self.name)
         t0 = time.time()
 
-        ### Start calculations
+        ### EL calculations
         louter.mainloop(self, )
-
         t1 = time.time()
+
+        ### Process data
+        df_lst, lst_meda = hf.final_fl_to_df(self.lst_pths_out)
+
+        ### Run Storage Model (optional)
+        if self.prms['storage_clc_iso']:
+            fin_df = self.clc_m.strg.clc_strg_state_iso(self, df_lst, )
+            df_lst = hd.slice_df_by_years(fin_df)
+        t2 = time.time()
+        dt0 = t1-t0 # time in seconds
+        dt1 = t2-t0 # time in seconds
+
+        ### Rewrite files
+        # TODO: Ensure correct order !
+        hf.update_metadata(lst_meda, dt0, dt1)
+        hf.rewrite_output_files(self, self.lst_pths_out, lst_meda, df_lst)
+
         #logging.info
         self.logger.info('End Simulation: %s', self.name)
-        dt = t1-t0 # time in seconds
+
         # TODO: update output data
 
         return None
