c9ef25cb5610e065bff7777f01930bd5d7a0258e
DFuh <dafu@uni-bremen.de>
2021-09-24 13:54:41 +0200

    update data handling, replace sig_df by input_df

--- epos/auxf/handlingdata.py
+++ epos/auxf/handlingdata.py
@@ -20,8 +20,8 @@ def check_sig_data(obj,):
         # read sig file -> metadata (dict), data (df)
 
         # sc_dct['sig'] yields name of sig (specified on sig_params)
-        sig_mtd, sig_df = rf.read_in_signal_dataset(obj, rel_flpth=obj.sig_par[sc_dct['sig']]['path'],
-                                                    search_key=obj.sig_par[sc_dct['sig']]['searchkey_sig_metadata'])#obj.sig_par['searchkey_sig_metadata'])
+        sig_mtd, sig_df = rf.read_in_dataset(obj, rel_flpth=obj.sig_par[sc_dct['input']]['path_sig'],
+                                                    search_key=obj.sig_par[sc_dct['input']]['searchkey_sig_metadata'])#obj.sig_par['searchkey_sig_metadata'])
         print('sIG METADATA: ', sig_mtd)
         # get data properties
         prop_dict = get_properties_df(sig_df)
@@ -75,7 +75,7 @@ def ini_data_output(obj):
     '''
     # make path
     obj.pth_data_out = hf.mirror_output_path(basename=obj.cwd,
-                                            ref_pth=obj.prms['refpth_sig_data'], #reference
+                                            ref_pth=obj.prms['refpth_input_data'], #reference
                                             filepath=obj.prms['relpth_sig_data'], # real sig path
                                             bsc_pth_out=obj.prms['bsc_pth_data_out'],
                                             tday=obj.today_ymd,name=obj.prms['scen_name'])
@@ -108,26 +108,41 @@ def ini_data_output(obj):
 
         #obj.flpth_data_out = obj.path_data_out+'/'+str(obj.tag) + nm
         flpth_out = os.path.join(pth_out, str(obj.tag)+nm)
-        df0_out, df_key_lst = mk_df_data_output(obj, dates)
+        df0_out, df0_cd, df_key_lst, unit_lst = mk_df_data_output(obj, dates)
 
         ###add row with units edit: DF, 20201203
         '''
         # -->>> TODO: get valid units !
         '''
-        units=[['[1]']*len(df0_out.columns)] # Spaceholder
+        # units=[['[1]']*len(df0_out.columns)] # Spaceholder
         '''
         <<<---
         '''
-        df_units = pd.DataFrame(data=units,columns=df0_out.columns, index=['unit'])
-        df_out = pd.concat([df_units, df0_out])
+        unit_lst[0] = '>units<'
+        df_units = pd.DataFrame(data=[unit_lst],columns=df0_out.columns, index=['unit'])
 
+
+
+        if n == 0: #
+            df0 = df0_out.copy()
+            lst0_df_keys = df_key_lst
+            # df_wr = df0_cd.copy()
+        else:
+            pass
+        df_wr = df0_out.copy()
+        df_out = pd.concat([df_units, df_wr]) # df_wr])
         ### following line creates output-file
         hf.mk_output_file(obj, yr, n, l, flpth_out, df_out, dates)
-        if n == 0: #???
-            df0 = df0_out
-            lst0_df_keys = df_key_lst
         lst_pths_out.append(flpth_out)
-    return df0, lst0_df_keys, lst_pths_out
+    #print('df0: ', df0.head())
+    return df0_cd, lst0_df_keys, lst_pths_out
+
+def slice_df_by_years(df):
+    df['years'] = df.index.year
+    df_lst = []
+    for key, dfi in df.groupby(by='years'):
+        df_lst.append(dfi)
+    return df_lst
 
 
 def mk_df_data_output(obj, dates):
@@ -136,26 +151,32 @@ def mk_df_data_output(obj, dates):
     '''
     key_lst = []
     full_lst = []
+    unit_lst = []
     #print(inst.s_parameters.data_output['varkeys'])
-    for key, val in obj.prms['output_parameters'].items():
+    default_data = [0]*len(obj.prms['output_parameters']) #np.zeros(len(key_lst))
+    for j,(key, val) in enumerate(obj.prms['output_parameters'].items()):
         #print(key, '--', val)
         full_lst.append(key)
         if val['store'] & obj.prms['select_stored_values']:
             key_lst.append(key)
+        default_data[j] = val.get("initial_value", 0)
+        unit_lst.append(val.get("unit", 'n/a'))
     #print('key-list for df0: ', key_lst)
-    default_data = [0]*len(full_lst) #np.zeros(len(key_lst))
+    # default_data = [0]*len(full_lst) #np.zeros(len(key_lst))
     #default_data[0] = pd.to_datetime(str(obj.sig.df['Date'].min().year))#'2020-01-01 00:00:00'
     default_data[0] = dates[0] #'2020-01-01 00:00:00'
+    print('default_data: ', default_data)
     #df0     = pd.DataFrame(data=[default_data], columns=full_lst) # full df
     #NT0 = namedtuple('NT0', full_lst)
     #nt0 = NT0(default_data)
     df0     = pd.DataFrame( columns=full_lst) # full df
+    df0_cd     = pd.DataFrame( data=[default_data], columns=full_lst) # full df
     #df_out  = pd.DataFrame(data=[default_data], columns=key_lst) # output (store) df
 
     if not obj.prms['select_stored_values']:
         key_lst = full_lst
     #df0['date'] = '2020-01-01 00:00:00'
-    return df0, key_lst#, full_lst
+    return df0, df0_cd, key_lst, unit_lst#, full_lst
 
 
 def ini_auxvals(obj, par):
@@ -165,6 +186,7 @@ def ini_auxvals(obj, par):
     '''
     @dataclass
     class AuxVals():
+        print('...Ini AuxVals()')
         d_mem = par['electrochemistry'].get('d0_mem', {}).get('value',None)
         c0_memrxn = par['electrochemistry'].get('c0_memrxn',{}).get('value',np.zeros(9)) # initial concentrations for membrane degradation reaction
         #if obj.pplnt.power_gradient_stack_max_pos:
@@ -181,9 +203,21 @@ def ini_auxvals(obj, par):
 
         t_ol = 0 # Overload time (counter) // in s
         t_nom = 0
-        dRct = 0 # ?
+        dRct = 1 # // in 1 !!!
 
+    @dataclass
+    class PressureVals():
+        print('...Ini PressureVals()')
+        anode_nom = par['operation']['nominal_electrode_pressure'].get('anode', {}).get('value',None)
+        cathode_nom = par['operation']['nominal_electrode_pressure'].get('cathode', {}).get('value',None)
+        anode = anode_nom
+        cathode = cathode_nom
+        pp_O2_mem_ca = par['operation']['initial_pressure_values'].get('pp_O2_ca', {}).get('value',0)
+        pp_O2_mem_an = par['operation']['initial_pressure_values'].get('pp_O2_an', {}).get('value',0)
+        pp_H2_mem_ca = par['operation']['initial_pressure_values'].get('pp_H2_ca', {}).get('value',0)
+        pp_H2_mem_an = par['operation']['initial_pressure_values'].get('pp_H2_an', {}).get('value',0)
     obj.av = AuxVals()
+    obj.p = PressureVals()
 
     return
 
