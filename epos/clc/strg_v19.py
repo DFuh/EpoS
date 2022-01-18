00c3b92cfa80abddd0ad8160f184fe4ac8a3a4e7
DFuh <dafu@uni-bremen.de>
2022-01-18 09:22:14 +0100

    storgae adjust

--- epos/clc/strg_v19.py
+++ epos/clc/strg_v19.py
@@ -19,9 +19,11 @@ def clc_strg_state_iso(obj, df_lst, ):
 
     full_df = pd.concat(df_lst)
     full_df['date'] = pd.to_datetime(full_df.date)
+    # print(full_df.head(10))
     dates = full_df.date
     full_df = full_df.set_index('date')
 
+    obj.logger.warning('Hardcoded Temp. and Press. (input); Storage Model (iso)')
     T_in = 313 # Temp of gas treatment
     p_in = 101325 # Pressure of Electrolyzer output // in Pa
 
@@ -34,26 +36,57 @@ def clc_strg_state_iso(obj, df_lst, ):
     t_span=[0,seconds_tot]
     print('t_span: ', t_span)
     # print('full_df: ', full_df.head(4))
-    flow_prd = full_df['n_H2_ca'].to_numpy() * obj.pec.M_H2 # Convert mol/s to kg/s
 
+
+    bal_df = full_df.copy()
+    bal_df['flow_H2_prd'] = bal_df['n_H2_ca'] * obj.pec.M_H2 # Convert mol/s to kg/s
+    bal_df['flow_H2_cns'] = bal_df['dm_H2_dmnd'] # kg/s
+    flow_cns = bal_df.flow_H2_cns.to_numpy()
+    flow_prd = bal_df.flow_H2_prd.to_numpy()
+    tdiff = full_df.t_abs.diff().to_numpy()
+    tdiff[0] = 0
+    # storage.cap_m
+    (bal_df['m_strg_sc'],
+    bal_df['m_dot_H2_grid'],
+    bal_df['m_dot_H2_strg']) =  xstrg.fill_strg(storage.m, storage.m_max, (flow_prd-flow_cns), tdiff)
     # TOD: add flow_demand !
-    flow_cns = full_df['n_H2_ca'].to_numpy()/3600
+    # flow_cns = full_df['n_H2_ca'].to_numpy()/3600
 
-    flow_bal = flow_prd-flow_cns
-    dm_in = np.where(flow_bal>0, flow_bal, 0)
-    dm_out = np.where(flow_bal<0, abs(flow_bal), 0)
+    # bal_df['flow_H2_prd_cums'] = bal_df.flow_H2_prd.cumsum()
+    # bal_df['flow_H2_cns_cums'] = bal_df.flow_H2_cns.cumsum()
 
-    ret, m_dot_act = storage.clc_state_mstp(T_in, p_in, t_span, dm_in, dm_out,
+    # bal_df['flow_H2_resid'] = bal_df.flow_H2_prd_cums - bal_df.flow_H2_cns_cums
+    # bal_df['m_strg_theo'] = bal_df.flow_H2_resid.cumsum()
+
+    #full_df['flow_H2_resid'] = np.where(
+    #                    (full_df.flow_H2_cns_cums - full_df.flow_H2_prd_cums) >0,
+    #                    full_df.flow_H2_cns, 0)
+    # bal_df['flow_H2_ext'] = np.where(bal_df.m_strg_theo<0,-bal_df.flow_H2_resid,0)
+    # flow_H2_ext = bal_df['flow_H2_ext'].to_numpy()
+
+    # flw_sum_prd = flow_prd + flow_H2_ext
+    # flow_bal = flow_prd - flow_cns
+    # dm_in = np.where(flow_bal>0, flow_bal, 0)
+    # dm_out = np.where(flow_bal<0, abs(flow_bal), 0)
+    dm_in = np.where(bal_df.m_dot_H2_strg>0, bal_df.m_dot_H2_strg, 0)
+    dm_out = np.where(bal_df.m_dot_H2_strg<0, abs(bal_df.m_dot_H2_strg), 0)
+
+    ret, m_dot_in_act = storage.clc_state_mstp(T_in, p_in, t_span, dm_in, dm_out,
                                         max_step=np.inf, heatloss_wall=True)
     full_df = full_df.assign(rho_strg=ret[0],
-                    u_strg=ret[1],
-                    T_Strg=ret[2],
-                    p_strg=ret[3],
-                    t_Strg=ret[4],
-                    m_strg=ret[5],
-                    U_strg=ret[6],
-                    Q_strg=ret[7],
-                    P_cmp=ret[8])
+                    u_strg=ret[1], # J/kg
+                    T_Strg=ret[2], # K
+                    p_strg=ret[3], # Pa
+                    t_Strg=ret[4], # h
+                    m_strg=ret[5], # kg
+                    U_strg=ret[6], # kJ
+                    Q_strg=ret[7], # kW
+                    P_cmp=ret[8], # kW
+                    dm_H2_to_strg_act=m_dot_in_act,   # kg/s
+                    dm_H2_to_strg=dm_in,        # kg/s
+                    dm_H2_from_strg=dm_out,     # kg/s
+                    dm_H2_ext=bal_df['m_dot_H2_grid'].to_numpy()
+                    )
 
     return full_df
 
