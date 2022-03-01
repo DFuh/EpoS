87ffd039b2e10289ba755d9e664bb21c92cfe95a
DFuh <dafu@uni-bremen.de>
2022-03-01 09:28:03 +0100

    corr run_plt (pp, m_ely)

--- epos/run_plr.py
+++ epos/run_plr.py
@@ -22,10 +22,11 @@ import epos.auxf.handlingfiles as hf
 import epos.auxf.readingfiles as rf
 
 
-def run_plr(scn_pth, T_spcs, i_spcs, p_spcs, mode=None, plot=True):
+def run_plr(scn_pth, T_spcs, i_spcs, p_spcs, mode=None, plot=True,
+            scn_dct=None, logpath=None):
 
     if plots:
-        sim = ElSim(scn_pth, full_simu=False)
+        sim = ElSim(scn_pth, full_simu=False, scn_dct=scn_dct, logpath=logpath)
         sim.setup_sim()
         #sim.logger.info(f'Scen_name: {sim.name}')
         sim.logger.info('Run plr test... mode= %s', mode)
@@ -52,6 +53,7 @@ def run_plr(scn_pth, T_spcs, i_spcs, p_spcs, mode=None, plot=True):
             print(f'No valid mode input (sys.argv = {mode})...')
 
         if plot:
+            plt_bsc_res(res_ca, T_in, i_arr, p_in,nfig=2201)
             plt_bsc_res(res_cell, T_in, i_arr, p_in)
             clc_plt_eff(res_cell, res_Urev, res_Utn, i_arr)
             clc_plt_Pcell(res_cell, i_arr)
@@ -115,23 +117,29 @@ def plr_sngl(sim, T_in, i_in, p_in):
     res_Pcell = np.zeros((lp, lT, li))
 
     for j in range(lp):
-
+        sim.p.anode, sim.p.cathode = p_in[j]
         for k in range(lT):
-
+            n_in = (0,0,0,0)
+            c_in = (0,0,0,0)
             for m in range(li):
                 sim.clc_m.aux.clc_auxvals(sim, T_in[k])
-                n_in = (0,0,0,0)
-                c_in = (0,0,0,0)
+
                 sim.av.fctr_n_c_A_abs = sim.pplnt.number_of_cells_in_plant_act * sim.pcll.active_cell_area
                 sim.av.fctr_n_c_A_st = sim.pplnt.number_of_cells_in_stack_act * sim.pcll.active_cell_area
-                m_ely = sim.bop.volumetricflow_ely_nominal * sim.pplnt.number_of_cells_in_stack_act
-                sim.clc_m.flws.materialbalance(sim,T_in[k],  i_in[m],
-                                    m_ely/sim.av.fctr_n_c_A_abs,
+                m_ely = sim.av.rho_ely*sim.bop.volumetricflow_ely_nominal * sim.pplnt.number_of_cells_in_stack_act
+                flws_o = sim.clc_m.flws.materialbalance(sim,T_in[k],  i_in[m],
+                                    m_ely/sim.av.fctr_n_c_A_st,
                                     sim.p, c_in, n_in,
                                     stf=sim.av.fctr_n_c_A_abs,
                                     ntd=None, sns=False, m=m)
-
-                results_plr= sim.clc_m.plr.voltage_cell(sim, sim.pec, T_in[k], i_in[m], p_in[j], pp=None, ini=True)
+                n_in = flws_o.n_H2_out_an, flws_o.n_H2_out_ca, flws_o.n_O2_out_an, flws_o.n_O2_out_ca
+                c_in = flws_o.c_H2_out_an, flws_o.c_H2_out_ca, flws_o.c_O2_out_an, flws_o.c_O2_out_ca
+                
+                pp = sim.p.pp_H2_mem_ca, sim.p.pp_O2_mem_an, 0 #pp_H2O
+                # pp = pp = sim.clc_m.flws.partial_pressure(sim, sim.pec, T_in[k], sim.p)
+                print('pp: ', pp)
+                #results_plr= sim.clc_m.plr.voltage_cell(sim, sim.pec, T_in[k], i_in[m], p_in[j], pp=pp, ini=True)
+                results_plr= sim.clc_m.plr.voltage_cell(sim, sim.pec, T_in[k], i_in[m], sim.p, pp=pp, ini=True)
                 res_Urev[j,k,m] = sim.clc_m.plr.cv_rev(sim, sim.pec, T_in[k], p_in[j])[1]
                 res_Utn[j,k,m] = sim.clc_m.plr.cv_rev(sim, sim.pec, T_in[k], p_in[j])[2]
                 res_cell[j,k,m]= results_plr[-1]
@@ -183,7 +191,7 @@ def setup_p(p_spcs):
         #p0_in.append([p0_ca, p0_an])
     return p0_in
 
-def plt_bsc_res(results, T_in, i_in, p_in):
+def plt_bsc_res(results, T_in, i_in, p_in, nfig=None):
     '''
     plot temp and pressure behaviour in two plots
     '''
@@ -196,7 +204,10 @@ def plt_bsc_res(results, T_in, i_in, p_in):
 
     lbs = [T_in, p_in]
     for n,(res, l) in enumerate(zip([temp_res, press_res], [lT,lp])):
-        plt.figure(n)
+        if nfig is not None:
+            plt.figure(nfig)
+        else:
+            plt.figure(n)
         if n == 0:
             lb = 1
         if n == 1:
@@ -204,6 +215,8 @@ def plt_bsc_res(results, T_in, i_in, p_in):
         for k in range(l):
             plt.plot(i_in, res[k,:], label=str(lbs[lb][0])+'_'+str(lbs[n][k]))
         plt.grid()
+        plt.xlabel('Current Density / $A/m²$')
+        plt.ylabel('Cell Voltage / $V$')
         plt.legend()
     plt.show()
     return
