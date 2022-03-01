89cb11672aba8645dbb3bcd43571cce9c62882d0
DFuh <dafu@uni-bremen.de>
2022-03-01 09:19:54 +0100

    adjust ael_dgr

--- epos/clc/ael_dgr_v20.py
+++ epos/clc/ael_dgr_v20.py
@@ -9,9 +9,9 @@ print(__name__ + ' imported...')
 def voltage_increase_lin(obj, pec, T, i):
 
     ### absolute voltage increase
-    dU_dgr_abs = obj.av.t_op/3600 * obj.pec.fctr_vlr # h * V/h
+    dU_dgr_act = obj.av.t_op/3600 * obj.pec.fctr_vlr # h * V/h
 
     ### incremental voltage increase
-    dU_dgr_incr = obj.av.t_diff/3600 * obj.pec.fctr_vlr # h * V/h
+    # dU_dgr_incr = obj.av.t_diff/3600 * obj.pec.fctr_vlr # h * V/h
 
-    return dU_dgr_incr, dU_dgr_abs
+    return dU_dgr_act, 0 #dU_dgr_abs
